a9d38a4f596fb75bcfa742d5f3038350813cbb8c
KimHyunJung <hday10166@gamil.com>
2023-08-25 15:00:36 +0900

    comment_delete

--- articles/models.py
+++ articles/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=10)
     content = models.TextField()
+    # comment_set 자동생성 (댓글들을 자동으로 연결하여 리스트로 출력해줌!)
 
 class Comment(models.Model):
+    #id , article_id 라는 foriegnkey 가 자동으로 생성된당!
     content = models.TextField()
     article = models.ForeignKey(Article, on_delete=models.CASCADE)    
 
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Article
+from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
 
 # Create your views here.
@@ -32,9 +32,22 @@ def detail(request, id):
     article = Article.objects.get(id=id)
     comment_form = CommentForm()
 
+    # # comment 목록 조회 
+    # # 첫번째 방법
+    # comment_list = Comment.objects.filter(article=article)
+
+    # # 두번째 방법
+    # comment_list = article.comment_set.all()
+
+    # 세번째 방법
+    # html 코드에서 article.comment_set.all로 사용!! 
+
+    #세번째>두번째>첫번째 순으로 권장
+
     context = {
         'article': article,
         'comment_form': comment_form,
+        # 'comment_list': comment_list,
     }
 
     return render(request, 'detail.html', context)
@@ -55,10 +68,19 @@ def comment_create(request, article_id):
         
         # 두번째 방법
         comment.article_id = article_id
-        
+
         # 비로소 저장
         comment.save()
 
         return redirect('articles:detail', id=article_id)
+
+
+
+def comment_delete(request, article_id, id):
+    comment = Comment.objects.get(id=id)
+    
+    comment.delete()
+
+    return redirect('articles:detail', id=article_id)
 
 
